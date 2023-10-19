f6fa232f1a30a6ffb2b999d4f989d6e901be4d0d
Braionic <trustedward@gmail.com>
2023-10-19 14:52:46 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
+import { type } from 'os';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  type namee = string;
+  type userage = number;
+  type person = {
+    name: String,
+    age: number,
+    lastName?: string
+  }
+
+  const Person: person = {
+    name: "henry",
+    age: 31
+  }
+  const man: {name: string, age: number} = {
+    name: "Rowland",
+    age: 32
+  }
+
+  function myf(name: string = "Steve", age: number = 50){
+    return `my name is ${name} and i am ${age} years old`
+  }
+  type men = string[];
+  let guys: men = ["ben, james"];
+
+  type women = [string, number];
+  let girls: women = ["mary", 19]
+  console.log(girls)
+  console.log(guys)
+  console.log(myf("Bob", 40))
+  console.log(man)
+  Person.lastName = "john";
+  console.log(Person)
+  let age: userage = 23;
+  console.log(age)
+
+  let name: namee = "man";
+  name = "woman";
+  console.log(name)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
